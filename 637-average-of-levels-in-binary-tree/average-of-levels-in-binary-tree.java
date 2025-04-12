081d2943593f6ffbb05a1cd5534c2df33b02102e
SOWRISH M S <159171743+SOWRISHMS@users.noreply.github.com>
2025-04-12 10:38:56 +0530

    Time: 4 ms (10.28%) | Memory: 45.7 MB (62.25%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
@@ -1,17 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>(List.of(root));
-        List<Double> ans = new ArrayList<>();
-        while (q.size() > 0) {
-            double qlen = q.size(), row = 0;
-            for (int i = 0; i < qlen; i++) {
-                TreeNode curr = q.poll();
-                row += curr.val;
-                if (curr.left != null) q.offer(curr.left);
-                if (curr.right != null) q.offer(curr.right);
+        Queue<TreeNode> st=new LinkedList<>();
+        st.offer(root);
+        List<Double> list =new ArrayList<>();
+        while(!st.isEmpty())
+        {   double sum=0;
+            List<Integer> lt =new ArrayList<>();
+            int size=st.size();
+            for(int i=0;i<size;i++)
+            {   TreeNode s=st.poll();
+                lt.add(s.val);
+                if(s.left!=null)
+                {
+                    st.offer(s.left);
+                }
+                if(s.right!=null)
+                {
+                    st.offer(s.right);
+                }
             }
-            ans.add(row/qlen);
+            for(int i:lt)
+            {
+                sum+=i;
+            }
+            int l=lt.size();
+            list.add((double)sum/l);
+
         }
-        return ans;
+        return list;
+        
+        
     }
+  
 }
